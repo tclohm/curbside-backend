f854c3314a0e9ed9b44014099a6ca02a3aead90e
Taylor Lohman <taylorlohman@duck.com>
2026-08-23 08:18:22 -0700

    pull lat and lng from photo metadata to show where photo was taken

---
+++ exif_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/rwcarlsen/goexif/exif"
+)
+
+func TestExifGPS(t *testing.T) {
+	f, err := os.Open("testdata/test.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lat, lng := x.LatLong()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("lat=%f lng=%f", lat, lng)
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/mattn/go-isatty v0.0.24 // indirect
 	github.com/ncruces/go-strftime v1.0.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	modernc.org/libc v1.74.4 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
